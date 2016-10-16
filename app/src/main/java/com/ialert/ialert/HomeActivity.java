851bb4f7ec862b448a1607b828c7c95d66055608
aashimagarg <agarg@utexas.edu>
2016-10-15 20:48:31 -0500

    newmereqeusts only

--- app/src/main/java/com/ialert/ialert/HomeActivity.java
+++ app/src/main/java/com/ialert/ialert/HomeActivity.java
@@ -9,16 +9,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
     private static final String TAG = "HomeActivity";
+    private static final int NUM_POSTS = 3;
     private FirebaseAuth.AuthStateListener authListener;
     private FirebaseAuth auth;
+    private static final String DATA = "data";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +77,98 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // network calls
+        final ArrayList<String> postIds = new ArrayList<String>();
+        final Map<String, Integer> friendsToCount = new HashMap<>();
+
+        // grab posts
+        GraphRequest requestPosts = GraphRequest.newMeRequest(
+                AccessToken.getCurrentAccessToken(), (GraphRequest.GraphJSONObjectCallback) new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/me/posts",
+                null,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+                        JSONArray jsonArray = response.getJSONObject().optJSONArray(DATA);
+                        if (jsonArray.length() != 0) {
+                            for (int i = 0; i < NUM_POSTS; i++) {
+                                postIds.add(jsonArray.optJSONObject(i).optString("id"));
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), getString(R.string.no_posts), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+        );
+        requestPosts.executeAsync();
+
+        // grab likes
+        for (int i = 0; i < NUM_POSTS; i++) {
+            GraphRequest requestLikes = GraphRequest.newMeRequest(
+                    AccessToken.getCurrentAccessToken(), (GraphRequest.GraphJSONObjectCallback) new GraphRequest(
+                            AccessToken.getCurrentAccessToken(),
+                            "/" + postIds.get(i) + "/likes",
+                            null,
+                            HttpMethod.GET,
+                            new GraphRequest.Callback() {
+                                public void onCompleted(GraphResponse response) {
+                                    JSONArray jsonArray = response.getJSONObject().optJSONArray(DATA);
+                                    if (jsonArray.length() != 0) {
+                                        for (int j = 0; j < jsonArray.length(); j++) {
+                                            String id = jsonArray.optJSONObject(j).optString("id");
+                                            if (!friendsToCount.containsKey(id)) {
+                                                // create new friend
+                                                friendsToCount.put(id, 1);
+                                            } else {
+                                                // increment count
+                                                friendsToCount.put(id, friendsToCount.get(id) + 1);
+                                            }
+                                        }
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), getString(R.string.no_likers), Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            })
+            );
+            requestLikes.executeAsync();
+        }
+
+        // grab comments
+        for (int i = 0; i < NUM_POSTS; i++) {
+            GraphRequest requestComments = GraphRequest.newMeRequest(
+                    AccessToken.getCurrentAccessToken(), (GraphRequest.GraphJSONObjectCallback) new GraphRequest(
+                            AccessToken.getCurrentAccessToken(),
+                            "/" + postIds.get(i) + "/comments",
+                            null,
+                            HttpMethod.GET,
+                            new GraphRequest.Callback() {
+                                public void onCompleted(GraphResponse response) {
+                                    JSONArray jsonArray = response.getJSONObject().optJSONArray(DATA);
+                                    if (jsonArray.length() != 0) {
+                                        for (int j = 0; j < jsonArray.length(); j++) {
+                                            String id = jsonArray.optJSONObject(j).optJSONObject("from").optString("id");
+                                            if (!friendsToCount.containsKey(id)) {
+                                                // create new friend
+                                                friendsToCount.put(id, 1);
+                                            } else {
+                                                // increment count
+                                                friendsToCount.put(id, friendsToCount.get(id) + 1);
+                                            }
+                                        }
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), getString(R.string.no_likers), Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            })
+            );
+            requestComments.executeAsync();
+        }
+
+        //store friends
+        DatabaseReference dataRef = FirebaseDatabase.getInstance().getReference("users/" + FirebaseAuth.getInstance().getCurrentUser().getUid().toString() + "/friends");
+        dataRef.setValue(friendsToCount);
 
     }
 
